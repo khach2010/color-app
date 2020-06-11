6ec97c62910c230b0336c1913c1853bca5c37497
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-06-11 20:23:12 +0200

    add unstyle slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.levelChange = this.levelChange.bind(this);
+  }
+  levelChange(level) {
+    this.setState({ level });
+  }
+
   render() {
-    const ColorBoxes = this.props.colors.map(color => (
-      <ColorBox background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+
+    const ColorBoxes = colors[level].map(color => (
+      <ColorBox background={color.hex} name={color.name} />
     ));
+
     return (
       <div className='Palette'>
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          onAfterChange={this.levelChange}
+          step={100}
+        />
         {/* Navbar goes here */}
         <div className='Palette-colors'>{ColorBoxes}</div>
         {/* footer goes here */}
