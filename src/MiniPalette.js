83d018a7cb37bc13485e64bf138639e70e302b52
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-06-15 11:19:29 +0200

    styling MiniPalette with JSS

---
+++ src/MiniPalette.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  main: {
+    backgroundColor: 'purple',
+    border: '3px solid teal'
+  },
+  secondary: {
+    backgroundColor: 'pink',
+    '& h1': {
+      color: 'white',
+      '& span': {
+        backgroundColor: 'yellow'
+      }
+    }
+  }
+};
+
+function MiniPalette(props) {
+  const { classes } = props;
+  console.log(classes);
+  return (
+    <div className={classes.main}>
+      <h1>Mini Palette</h1>
+      <section className={classes.secondary}>
+        <h1>
+          MiniPalette<span>sdasdda</span>
+        </h1>
+        <span>sdasdda</span>
+      </section>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MiniPalette);
