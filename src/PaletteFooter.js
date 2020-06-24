ca3af292e2f5adfe8e95a269e577ca2eada5dada
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-06-24 20:31:46 +0200

    Move Out Styles

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import styles from './styles/PaletteFooterStyles';
+import { withStyles } from '@material-ui/core/styles';
 
 function PaletteFooter(props) {
-  const { paletteName, emoji } = props;
+  const { paletteName, emoji, classes } = props;
   return (
-    <footer className='Palette-footer'>
-      {paletteName} <span className='emoji'>{emoji}</span>
+    <footer className={classes.PaletteFooter}>
+      {paletteName} <span className={classes.emoji}>{emoji}</span>
     </footer>
   );
 }
 
-export default PaletteFooter;
+export default withStyles(styles)(PaletteFooter);
