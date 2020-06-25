11e628c2c1de4ff11ea59771beed27461e07fd67
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-06-25 19:52:12 +0200

    Refactor Navbar styles and remove css

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,10 +5,11 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/NavbarStyles';
 
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
 
 class Navbar extends Component {
   constructor(props) {
@@ -27,17 +28,17 @@ class Navbar extends Component {
   }
 
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showingAllColors, classes } = this.props;
     const { format } = this.state;
     return (
-      <header className='Navbar'>
-        <div className='logo'>
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to='/'>ReactColorPicker</Link>
         </div>
-        {this.props.showingAllColors && (
-          <div className='slider-container'>
+        {showingAllColors && (
+          <div>
             <span>Level: {level}</span>
-            <div className='slider'>
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -48,7 +49,7 @@ class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className='select-container'>
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value='hex'>HEX - #ffffff</MenuItem>
             <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
@@ -82,4 +83,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
