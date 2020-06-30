088239f5cd94dc56fc100ab89cb9acb4f229a3ed
khach2010 <41897918+khach2010@users.noreply.github.com>
2020-06-30 20:05:44 +0200

    add Delete to NewPaletteForm

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -151,6 +151,12 @@ class NewPaletteForm extends Component {
     this.props.history.push('/');
   }
 
+  removeColor(colorName) {
+    this.setState({
+      colors: this.state.colors.filter(color => color.name !== colorName)
+    });
+  }
+
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -249,7 +255,12 @@ class NewPaletteForm extends Component {
           <div className={classes.drawerHeader}>
             <ul className={classes.ulHeight}>
               {this.state.colors.map(color => (
-                <DraggableColorBox color={color.color} name={color.name} />
+                <DraggableColorBox
+                  key={color.name}
+                  color={color.color}
+                  name={color.name}
+                  handleClick={() => this.removeColor(color.name)}
+                />
               ))}
             </ul>
           </div>
